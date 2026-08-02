fabb7bc097456a4d2e37a2490dafef9232da5cf7
yagiuec17 <yagi.uec17@gmail.com>
2026-08-03 02:18:51 +0900

    初回

---
+++ www/card_list/card_prb01.js
@@ -0,0 +1,17 @@
+const CARD_PRB01 = [
+    {
+        name:"サンジ",
+        number:"PRB01-001",
+        img:"card/PRB01/PRB01-001.png",
+        color:["赤"],
+        type:"LEADER",
+        block:3,
+        cost:5,
+        power:5000,
+        attribute:"打",
+        counter:0,
+        feature:"麦わらの一味",
+        text:"【起動メイン】【ターン1回】自分のコスト8以下の【登場時】効果を持たないキャラ1枚までは、このターン中、【速攻】を得る。(このカードは登場したターンにアタックできる)",
+        trigger:"-"
+        }
+    ];
